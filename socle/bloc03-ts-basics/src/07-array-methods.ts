9f464bce1ecb5fcbf3751f769c9ce85d11b074a8
Solofoniaina Armando <hello@koulier.com>
2026-09-21 15:06:32 +0200

    Bloc 3 · quête 7 : La chaîne de montage

--- socle/bloc03-ts-basics/src/07-array-methods.ts
+++ socle/bloc03-ts-basics/src/07-array-methods.ts
@@ -3,27 +3,31 @@
 import type { Movie } from "./types.ts";
 
 export function titles(movies: Movie[]): string[] {
-  // TODO
+  return movies.map((movie) => movie.title);
 }
 
 export function wellRated(movies: Movie[], minimum = 4): Movie[] {
-  // TODO
+  return movies.filter((movie) => (movie.rating ?? 0) >= minimum);
 }
 
 export function totalRuntime(movies: Movie[]): number {
-  // TODO
+  return movies.reduce((total, movie) => total + (movie.runtime ?? 0), 0);
 }
 
 export function findByTitle(movies: Movie[], title: string): Movie | undefined {
-  // TODO
+  return movies.find((movie) => movie.title === title);
 }
 
 export function allKidFriendly(movies: Movie[]): boolean {
-  // TODO
+  return movies.every((movie) => (movie.minimumAge ?? 0) <= 10);
 }
 
 export function anyNew(movies: Movie[]): boolean {
-  // TODO
+  return movies.some((movie) => movie.isNew === true);
 }
 
-// Challenge ⭐: marquee(movies): string
+export function marquee(movies: Movie[]): string {
+  return wellRated(movies)
+    .map((movie) => movie.title.toUpperCase())
+    .join(" · ");
+}
